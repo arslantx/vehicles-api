71a59cd642eeef40fcb5f0b55de1a8bf7a8c4d62
Arslan Haytiyev <arslanh@gmail.com>
2020-10-25 14:23:36 -0500

    Converting pricing service to microservice

--- pricing-service/src/main/java/com/udacity/pricing/entity/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/entity/Price.java
@@ -1,15 +1,24 @@
-package com.udacity.pricing.domain.price;
+package com.udacity.pricing.entity;
 
 import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "VEHICLEID")
+    private Long vehicleId;
     private String currency;
     private BigDecimal price;
-    private Long vehicleId;
 
     public Price() {
     }
---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Price (vehicleId, currency, price) VALUES (1, 'USD', 15200.25);
+INSERT INTO Price (vehicleId, currency, price) VALUES (2, 'USD', 28250.77);
+INSERT INTO Price (vehicleId, currency, price) VALUES (3, 'USD', 19000.00);
+INSERT INTO Price (vehicleId, currency, price) VALUES (4, 'USD', 13050.99);
+INSERT INTO Price (vehicleId, currency, price) VALUES (5, 'USD', 15200.25);
